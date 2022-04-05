d36b03e5ca43db9050718ee3d473647bb0d5597b
DINGU <web.ljg@gmail.com>
2022-04-05 15:49:40 +0900

    # nuxtServerInit을 활용한 장바구니 목록 추가, 조회 기능 API 연동

--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -11,7 +11,7 @@
       <div class="side-panel">
         <p class="name">name: {{ product.name }}</p>
         <p class="price">price: {{ product.price }}</p>
-        <button type="button" @click="addToCart">Add to Cart</button>
+        <button type="button" @click="addToCart">카트에 담기</button>
       </div>
     </div>
   </div>
@@ -19,7 +19,7 @@
 
 <script>
 // import axios from 'axios';
-import { fetchProductById } from '@/api/index';
+import { createCartItem, fetchProductById } from '@/api/index';
 
 export default {
   async asyncData({ params }) {
@@ -29,7 +29,9 @@ export default {
     return { product };
   },
   methods: {
-    addToCart() {
+    async addToCart() {
+      const response = await createCartItem(this.product);
+      console.log(response);
       this.$store.commit('addCartItem', this.product);
       this.$router.push('/cart');
     },
--- store/index.js
+++ store/index.js
@@ -1,3 +1,10 @@
+import { fetchCartItems } from '~/api';
+
+// constants
+export const FETCH_CART_ITEMS = 'FETCH_CART_ITEMS';
+
+// store
+
 export const state = () => ({
   cartItems: [],
 });
@@ -9,5 +16,32 @@ export const mutations = {
       imageUrl: `${cartItem.imageUrl}?random=${Math.random()}`,
     };
     state.cartItems.push(newCartItem);
+  },
+  setCartItems(state, cartItems) {
+    state.cartItems = cartItems;
+  },
+};
+
+export const actions = {
+  async [FETCH_CART_ITEMS]({ commit }) {
+    const { data } = await fetchCartItems();
+    commit(
+      'setCartItems',
+      data.map(item => ({
+        ...item,
+        imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+      })),
+    );
+  },
+  async nuxtServerInit(storeContext, nuxtContext) {
+    await storeContext.dispatch(FETCH_CART_ITEMS);
+    // const { data } = await fetchCartItems();
+    // storeContext.commit(
+    //   'setCartItems',
+    //   data.map(item => ({
+    //     ...item,
+    //     imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+    //   })),
+    // );
   },
 };
